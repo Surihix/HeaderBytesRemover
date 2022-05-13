27a65640420bd4f3587f7b30035b450eb41cd845
Sura Shion <neweramysterymailer@gmail.com>
2022-05-13 21:13:47 +0530

    no-text-file-used

--- Program.cs
+++ Program.cs
@@ -10,69 +10,40 @@ namespace HeaderBytesRemover
     {
         static void Main(string[] args)
         {
-            // Create three lists to store the text files and the data in it as strings  
-            var list1 = new List<string>();
-            var list2 = new List<string>();
-            var list3 = new List<string>();
 
+                  Console.WriteLine("Enter your input filename with extension:");
 
-            // Open the infile_name.txt file in a streamreader and store it in a list
-            using (var streamReaderinfile = new StreamReader("infile_name.txt", Encoding.UTF8))
-            {
-                string infilename;
-                while ((infilename = streamReaderinfile.ReadLine()) != null)
-                {
-                    list1.Add(infilename);
+                  // Open the file that the user has entered on the console in a filestream
+                  using (Stream InFile = new FileStream(Console.ReadLine(), FileMode.Open, FileAccess.Read))
+                     {
 
-                    // Open the file defined in the infile_name.txt file in a filestream
-                    using (Stream InFile = new FileStream(infilename, FileMode.Open, FileAccess.Read))
-                    {
+                        Console.WriteLine("\nInput the byte position: ");
 
-
-                        // Open the byte_position.txt file in streamreader and store it in a list
-                        using (var streamReaderByteposFile = new StreamReader("byte_position.txt", Encoding.UTF8))
+                        if (int.TryParse(Console.ReadLine(), out int bytenumber))
                         {
-                            string bytePosfilename;
-                            while ((bytePosfilename = streamReaderByteposFile.ReadLine()) != null)
-                            {
-                                list2.Add(bytePosfilename);
+                            // Seek to the byte specified in the byte_position.txt file 
+                            InFile.Seek(bytenumber, SeekOrigin.Begin);
 
+                            Console.WriteLine("\nRemoving header bytes....");
 
-                                // Convert the strings read in the byte_position.txt file to int
-                                if (int.TryParse(bytePosfilename, out int bytenumber))
-                                {
-                                    // Seek to the byte specified in the byte_position.txt file 
-                                    InFile.Seek(bytenumber, SeekOrigin.Begin);
+                            Console.WriteLine("\nEnter your output filename with extension: ");
 
+                            // Create the file that the user has entered on the console in a filestream
+                            FileStream HeaderstrippedOutFile = new FileStream(Console.ReadLine(), FileMode.CreateNew);
 
-                                    Console.WriteLine("Removing header bytes....");
+                            // Copy the bytes to the output file filestream  
+                             _ = new MemoryStream();
+                             InFile.CopyTo(HeaderstrippedOutFile);
+                                                             
 
-
-                                    // Open the outfile_name.txt file in a streamreader and store it in a list.                                  
-                                    using (var streamReaderOutfile = new StreamReader("outfile_name.txt", Encoding.UTF8))
-                                    {
-                                        string outfilename;
-                                        while ((outfilename = streamReaderOutfile.ReadLine()) != null)
-                                        {
-                                            list3.Add(outfilename);
-
-
-                                            // Create the file defined in the outfile_name.txt file in a filestream
-                                            FileStream HeaderstrippedOutFile = new FileStream(outfilename, FileMode.CreateNew);
-
-                                            // Copy the bytes to the output file filestream  
-                                            _ = new MemoryStream();
-                                            InFile.CopyTo(HeaderstrippedOutFile);
-
-                                        }
-                                    }
-                                }
-                            }
                         }
-                    }
-                }
-            }
+
+                  }
+        
+                
         }
-    }
+    
+    }    
+
 }
 
