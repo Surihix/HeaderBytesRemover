fbb1d39a8cdbfe5ffb6c643418c99293c2e65fe7
Sura Shion <neweramysterymailer@gmail.com>
2022-06-26 01:13:57 +0530

    use-arguments-fixes

--- Program.cs
+++ Program.cs
@@ -7,12 +7,12 @@ namespace HeaderBytesRemover
     {
         static void Main(string[] args)
         {
-            
+
             // Check the number of arguements specified by the user 
             // and if its less than 3, then terminate the program
             if (args.Length != 3)
             {
-               return;
+                return;
             }
 
             // Storing the values specified in each of the arguements as variables
@@ -20,28 +20,28 @@ namespace HeaderBytesRemover
             long byte_pos = Convert.ToInt64(args[1]);
             string output_file_arg = args[2];
 
-                        if (int.TryParse(Console.ReadLine(), out int bytenumber))
-                        {
-                            // Seek to the byte specified in the byte_position.txt file 
-                            InFile.Seek(bytenumber, SeekOrigin.Begin);
 
-               // Seek to the byte value specified in arg[1]
-               InFile.Seek(byte_pos, SeekOrigin.Begin);
+            // Open the input file specified in arg[0] in a filestream
+            using (Stream InFile = new FileStream(input_file_arg, FileMode.Open, FileAccess.Read))
+            {
 
-               // Create the output file specified in arg[2] in a filestream
-               FileStream OutFile = new FileStream(output_file_arg, FileMode.CreateNew);
+                // Seek to the byte value specified in arg[1]
+                InFile.Seek(byte_pos, SeekOrigin.Begin);
 
-               Console.WriteLine("\nRemoving header bytes....");
-                            
-               // Copy the bytes to the output file filestream  
-               _ = new MemoryStream();
-               InFile.CopyTo(OutFile);
-                                                             
+                // Create the output file specified in arg[2] in a filestream
+                FileStream OutFile = new FileStream(output_file_arg, FileMode.CreateNew);
+
+                Console.WriteLine("\nRemoving header bytes....");
+
+                // Copy the bytes to the output file filestream  
+                _ = new MemoryStream();
+                InFile.CopyTo(OutFile);
+
             }
 
-        }                        
-        
+        }
+
     }
-    
-}    
+
+}
 
